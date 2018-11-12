e337da6f6ab038651ac656fb4f06112ceb4c205b
rravik <rravikumar777@gmail.com>
2018-11-13 00:39:34 +0530

    Added gitTesting

--- src/test/java/SeleniumLearning/DataDrivenTest.java
+++ src/test/java/SeleniumLearning/DataDrivenTest.java
@@ -25,6 +25,7 @@ public class DataDrivenTest {
 		
 		
 
+		//Git purpose
 	}
 
 }
---
+++ src/test/java/SeleniumLearning/gitTesting.java
@@ -0,0 +1,11 @@
+package SeleniumLearning;
+
+public class gitTesting {
+	
+	public void gitSample()
+	{
+		System.out.println("Git Sample Test");
+		
+	}
+
+}
